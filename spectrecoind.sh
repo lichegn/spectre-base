4ee0e9562155c64fada3a062aedd44bb7795803d
HLXEasy <hlxeasy@gmail.com>
2018-07-30 16:39:39 +0200

    Improved entrypoint script to handle required entries on spectrecoin.conf

--- spectrecoind.sh
+++ spectrecoind.sh
@@ -7,11 +7,32 @@
 #
 # ============================================================================
 
-if [ -n "${RPCUSER}" ] ; then
-    sed -i "s/rpcuser=.*$/rpcuser=${RPCUSER}/g" $HOME/.spectrecoin/spectrecoin.conf
-fi
-if [ -n "${RPCPASSWORD}" ] ; then
-    sed -i "s/rpcpassword=.*$/rpcpassword=${RPCUSER}/g" $HOME/.spectrecoin/spectrecoin.conf
+if [ -e $HOME/.spectrecoin/spectrecoin.conf ] ; then
+    # spectrecoin.conf found, try to update it
+    if $(grep -q "rpcuser=" $HOME/.spectrecoin/spectrecoin.conf) ; then
+        # Var rpcuser found, update it
+        if [ -n "${RPCUSER}" ] ; then
+            sed -i "s/rpcuser=.*$/rpcuser=${RPCUSER}/g" $HOME/.spectrecoin/spectrecoin.conf
+        fi
+    else
+        # Var rpcuser not found but required, create it
+        echo "rpcuser=spectrecoinrpc" >> $HOME/.spectrecoin/spectrecoin.conf
+    fi
+    if $(grep -q "rpcpassword=" $HOME/.spectrecoin/spectrecoin.conf) ; then
+        # Var rpcuser found, update it
+        if [ -n "${RPCPASSWORD}" ] ; then
+            sed -i "s/rpcpassword=.*$/rpcpassword=${RPCPASSWORD}/g" $HOME/.spectrecoin/spectrecoin.conf
+        fi
+    else
+        # Var rpcpassword not found but required, create it
+        rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 44 | head -n 1)
+        echo "rpcpassword=${rpcpassword}" >> $HOME/.spectrecoin/spectrecoin.conf
+    fi
+else
+    # spectrecoin.conf not found, create it with minimal content
+    echo "rpcuser=spectrecoinrpc" > $HOME/.spectrecoin/spectrecoin.conf
+    rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 44 | head -n 1)
+    echo "rpcpassword=${rpcpassword}" >> $HOME/.spectrecoin/spectrecoin.conf
 fi
 
 /usr/local/bin/spectrecoind
